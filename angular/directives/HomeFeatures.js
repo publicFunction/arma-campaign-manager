e5dc320f47c34caf28e61e4898275855ef400126
Chris Moss <chris@publicfunction.co.uk>
2016-07-24 13:26:54 +0100

    Added responsive to home for hover cta
    Signed-off-by: Chris Moss <chris@publicfunction.co.uk>

--- angular/directives/HomeFeatures.js
+++ angular/directives/HomeFeatures.js
@@ -1,28 +1,37 @@
 application.directive('homeFeatures', function() {
+    var self = this;
+    reveals = function (evt, elem) {
+        reveal = elem.find('div.reveal');
+        content = reveal.find('div.content');
+        switch (evt.type) {
+            case "click":
+            case "mouseenter":
+                reveal.show();
+                $.each(content, function() {
+                    if($(this).attr('href') == '#'+evt.currentTarget.id) {
+                        $(this).fadeIn('500');
+                    }
+                });
+                break;
+            case "mouseleave":
+                reveal.hide();
+                content.hide();
+                break;
+        }
+    };
     return {
         element: 'E',
         templateUrl : 'templates/partials/home_features.html',
         link: function(scope, elem, attr) {
             articles = elem.find('article');
             articles.hover(function (evt) {
-                reveal = elem.find('div.reveal');
-                content = reveal.find('div.content');
-                switch (evt.type) {
-                    case "mouseenter":
-                        reveal.show();
-                        $.each(content, function() {
-                            if($(this).attr('href') == '#'+evt.currentTarget.id) {
-                                $(this).fadeIn('500');
-                            }
-                        });
-                        break;
-                    case "mouseleave":
-                        reveal.hide();
-                        content.hide();
-                        break;
-                }
+                evt.preventDefault();
+                self.reveals(evt, elem);
             });
-
+            articles.click(function(evt) {
+                evt.preventDefault();
+                self.reveals(evt, elem);
+            })
         }
     }
 });
